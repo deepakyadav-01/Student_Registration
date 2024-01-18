1b6b64dc3ce065edab1e231053069bc806c99f44
deepakyadav-01 <deepak.y@mindfiresolutions.com>
2024-01-18 12:00:16 +0530

    bugs cleared

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -58,6 +58,8 @@ const Form = ({ onFormSubmit, selectedStudent }) => {
 
     if (isFormValid) {
       onFormSubmit(e, formData);
+      setFormData(initialFormData);
+
     }
   };
   const handleReset = () => {

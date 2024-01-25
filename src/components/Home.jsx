4e96181118ae31830f57b85f57883e4f5cc8c836
deepakyadav-01 <155735643+deepakyadav-01@users.noreply.github.com>
2024-01-25 11:27:55 +0530

    edit toast added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -71,6 +71,16 @@ const Home = () => {
     console.log("Selected Student:", selectedStudent);
   
     if (selectedStudent) {
+            toast.success("Data entered to form!", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       // Use the spread operator to create a shallow copy of the selected student data
       const updatedFormData = { ...selectedStudent };
   
